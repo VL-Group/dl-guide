51fbf4d6c2a516f22a6bc01f148ef2327bde3ad2
Xiaosu Zhu <Xiaosu.Zhu@outlook.com>
2020-07-18 16:30:57 +0800

    Create set.sh

---
+++ Manuals/set.sh
@@ -0,0 +1,20 @@
+
+# find . -maxdepth 1 -type d | sudo bash ~/set.sh
+while read line;
+do
+	if [[ "$line" == *"+"* ]]
+	then
+		continue	
+	fi
+	if [[ "$line" == *" "* ]]
+	then 
+		continue
+	fi
+	if [[ "$line" == "." ]]
+	then
+		continue
+	fi
+	user=${line:2}
+    chown -R $user:$user $user
+	echo "Change owner of $user completed"	
+done
